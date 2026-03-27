f089dfbb6cd3e134a0eb9b421c624a8c2840b16f
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-03-27 16:13:40 -0300

    feat: adiciona estrutura inicial

---
+++ src/main/java/com/ctw/spring_academic_dto_pattern/SpringAcademicDtoPatternApplication.java
@@ -0,0 +1,13 @@
+package com.ctw.spring_academic_dto_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademicDtoPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAcademicDtoPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctw/spring_academic_dto_pattern/SpringAcademicDtoPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctw.spring_academic_dto_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademicDtoPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
